aec980f593e0a08203bd523445fa73d0a81137cb
wzb841014 <50065751+wzb841014@users.noreply.github.com>
2026-03-02 09:57:06 +0800

    Update display_gc9a01a.c

--- drivers/display/display_gc9a01a.c
+++ drivers/display/display_gc9a01a.c
@@ -12,10 +12,10 @@
 
 #include "display_gc9a01a.h"
 
-#include <zephyr/dt-bindings/display/gc9a01a.h>
-#include <zephyr/drivers/display.h>
-#include <zephyr/sys/byteorder.h>
-#include <zephyr/logging/log.h>
+#include <dt-bindings/display/gc9a01a.h>
+#include <drivers/display.h>
+#include <sys/byteorder.h>
+#include <logging/log.h>
 
 LOG_MODULE_REGISTER(display_gc9a01a, CONFIG_DISPLAY_LOG_LEVEL);
 
